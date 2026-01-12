f793984ad7db70775037e923c7617ebdf2688e19
Steve Davidson <stevedavidson1@gmail.com>
2026-01-12 20:01:03 +0800

    Version 2.0: Added user name personalization

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 import random
 
-# My first GitHub App
-
-# A list of quotes to choose from
+# Version 2.0: Personalizing the motivation
 quotes = [
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "Your passion is waiting for your courage to catch up. - Isabelle Lafleche",
-    "The best way to predict the future is to invent it. - Alan Kay",
-    "Stay hungry, stay foolish. - Steve Jobs"
+    "Believe you can and you're halfway there.",
+    "The best way to predict the future is to invent it.",
+    "Stay hungry, stay foolish."
 ]
 
 def show_motivation():
-    # Pick one random quote from the list
+    name = input("Enter your name for a custom quote: ")
     daily_quote = random.choice(quotes)
-    print("\n--- DAILY MOTIVATION ---")
+    
+    print(f"\n--- {name.upper()}'S DAILY MOTIVATION ---")
     print(daily_quote)
-    print("------------------------\n")
+    print("--------------------------------------\n")
 
 if __name__ == "__main__":
     show_motivation()
