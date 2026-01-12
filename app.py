db6c39111431ffd8fa613746a6c9771eff961c4b
Steven Davidson <stevendavidson@Stevens-MacBook-Pro.local>
2026-01-12 19:41:23 +0800

    Initial commit: My first motivational app

---
+++ app.py
@@ -0,0 +1,19 @@
+import random
+
+# A list of quotes to choose from
+quotes = [
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "Your passion is waiting for your courage to catch up. - Isabelle Lafleche",
+    "The best way to predict the future is to invent it. - Alan Kay",
+    "Stay hungry, stay foolish. - Steve Jobs"
+]
+
+def show_motivation():
+    # Pick one random quote from the list
+    daily_quote = random.choice(quotes)
+    print("\n--- DAILY MOTIVATION ---")
+    print(daily_quote)
+    print("------------------------\n")
+
+if __name__ == "__main__":
+    show_motivation()
